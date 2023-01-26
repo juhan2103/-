1ce571d8ef8ad86a6c0cc13638e03cff752ad292
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-01-26 20:35:11 +0900

    join page 구현

---
+++ app/src/main/java/com/pronunu/mysololife/auth/JoinActivity.kt
@@ -0,0 +1,15 @@
+package com.pronunu.mysololife.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pronunu.mysololife.R
+
+class JoinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+
+    }
+}
