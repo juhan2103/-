6bf11c8870f002d58db36a769015e2ff31cf490a
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-01-26 20:34:51 +0900

    login page 구현

---
+++ app/src/main/java/com/pronunu/mysololife/auth/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.pronunu.mysololife.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pronunu.mysololife.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
