9ddb5c81ad194f3d87db89a0ba3571ab66dc2c60
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-01-26 20:34:22 +0900

    intro page 구현

--- app/src/main/java/com/pronunu/mysololife/auth/IntroActivity.kt
+++ app/src/main/java/com/pronunu/mysololife/auth/IntroActivity.kt
@@ -1,12 +1,29 @@
 package com.pronunu.mysololife.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.pronunu.mysololife.R
+import com.pronunu.mysololife.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+
+        binding.loginBtn.setOnClickListener {
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.joinBtn.setOnClickListener {
+            var intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
