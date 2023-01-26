0327e4690fcbb7ec1479d7a557eea01dba463420
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-01-26 18:12:01 +0900

    splash activity 구현

---
+++ MySoloLife/app/src/main/java/com/pronunu/mysololife/auth/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.pronunu.mysololife.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pronunu.mysololife.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
