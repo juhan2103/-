940c17fa9e66488f41b5bc8174fb5adbef1a03a5
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-01-30 23:05:29 +0900

    로그인, 로그아웃 기능 구현

--- app/src/main/java/com/pronunu/mysololife/auth/LoginActivity.kt
+++ app/src/main/java/com/pronunu/mysololife/auth/LoginActivity.kt
@@ -1,15 +1,48 @@
 package com.pronunu.mysololife.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.pronunu.mysololife.MainActivity
 import com.pronunu.mysololife.R
+import com.pronunu.mysololife.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
 
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        binding.loginBtn.setOnClickListener {
+
+            val email = binding.emailArea.text.toString()
+            val password = binding.passwordArea.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        startActivity(intent)
+
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+
+                    } else {
+                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
     }
 }
