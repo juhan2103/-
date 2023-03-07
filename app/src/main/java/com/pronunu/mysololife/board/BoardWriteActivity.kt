919959a9d136b157ffcba9067cd19e59a4ea0eca
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-03-07 17:38:02 +0900

    게시글 업로드 액티비티 구현

---
+++ app/src/main/java/com/pronunu/mysololife/board/BoardWriteActivity.kt
@@ -0,0 +1,49 @@
+package com.pronunu.mysololife.board
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.pronunu.mysololife.R
+import com.pronunu.mysololife.databinding.ActivityBoardWriteBinding
+import com.pronunu.mysololife.utils.FBAuth
+import com.pronunu.mysololife.utils.FBRef
+
+class BoardWriteActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityBoardWriteBinding
+
+    private val TAG = BoardWriteActivity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_board_write)
+
+        binding.writeBtn.setOnClickListener {
+
+            val title = binding.titleArea.text.toString()
+            val content = binding.contentArea.text.toString()
+            val uid = FBAuth.getUid()
+            val time = FBAuth.getTime()
+
+            Log.d(TAG, title)
+            Log.d(TAG, content)
+
+            // board
+            //  - key
+            //   - boardModel(title, content, uid, time)
+
+            FBRef.boardRef
+                .push()
+                .setValue(BoardModel(title, content, uid, time))
+
+            Toast.makeText(this, "게시글 입력 완료", Toast.LENGTH_LONG).show()
+
+            finish()
+
+        }
+
+    }
+}
