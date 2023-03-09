e36d2b4b1e87f10507d379ea4dabf70f7f01e366
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-03-09 23:28:41 +0900

    이미지 불러오기 기능 구현

--- app/src/main/java/com/pronunu/mysololife/board/BoardInsideActivity.kt
+++ app/src/main/java/com/pronunu/mysololife/board/BoardInsideActivity.kt
@@ -3,11 +3,15 @@ package com.pronunu.mysololife.board
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.pronunu.mysololife.R
 import com.pronunu.mysololife.databinding.ActivityBoardInsideBinding
 import com.pronunu.mysololife.utils.FBRef
@@ -43,7 +47,27 @@ class BoardInsideActivity : AppCompatActivity() {
 
         val key = intent.getStringExtra("key")
         getBoardData(key.toString())
+        getImageData(key.toString())
 
+    }
+
+    private fun getImageData(key : String){
+        // Reference to an image file in Cloud Storage
+        val storageReference =  Firebase.storage.reference.child(key + ".jpeg")
+
+        // ImageView in your Activity
+        val imageViewFromFB = binding.getImageArea
+
+        storageReference.downloadUrl.addOnCompleteListener( {task ->
+            if(task.isSuccessful){
+                Glide.with(this)
+                    .load(task.result)
+                    .into(imageViewFromFB)
+            }
+            else{
+
+            }
+        })
     }
 
     private fun getBoardData(key : String){
